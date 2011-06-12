174357686d8b6b72e8507fe8fab2c66594d3429e
Robert Greiner <robert@robertgreiner.com>
2011-06-12 09:21:29 -0500

    Clear out textarea on initial focus

--- mincss/mincss.js
+++ mincss/mincss.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
 
+    var defaultString = 'Enter your CSS code here.';
+    $('#userInput').val(defaultString);
+
+    $('#userInput').click(function () {
+        if ($(this).val() == defaultString) {
+            $(this).val('');
+        }
+    });
+
 });
